83c1cbba504d6d8a27e72d3c5d6108ba7cb632e3
Goodwill T Mumvenge <36727562+gmvenge@users.noreply.github.com>
2018-03-24 17:01:21 +0200

    Friends and followers added

--- goowillsentiment.py
+++ goowillsentiment.py
@@ -45,8 +45,53 @@ UserTimeline = api.GetUserTimeline(screen_name = ScreenName)
 ]
 
 # parse the result using JSON library in Python
+ScreenName = "gmvenge"
+Friends = GetFriends(screen_name = ScreenName)
+Followers = GetFollowers(screen_name = ScreenName)
+"users": [
+    {
+      "profile_sidebar_fill_color": "252429",
+      "profile_sidebar_border_color": "181A1E",
+      "profile_background_tile": false,
+      "name": "Ezekiel Munyawiri",
+      "profile_image_url": "http://a0.twimg.com/profile_images/2838630046/4b82e286a659fae310012520f4f756bb_normal.png",
+      "created_at": "Thu Jan 18 00:10:45 +0000 2012",
+      "location": "Harare",
+      "follow_request_sent": false,
+      "profile_link_color": "2FC2EF",
+      "is_translator": false,
+      "id_str": "657693",
+      "default_profile": false,
+      "contributors_enabled": true,
+      "favourites_count": 1973,
+      "url": "http://afroginthevalley.com/",
+      "profile_banner_url": "https://si0.twimg.com/profile_banners/657693/1353537845",
+      "profile_image_url_https": "https://si0.twimg.com/profile_images/2838630046/4b82e286a659fae310012520f4f756bb_normal.png",
+      "utc_offset": -18000,
+      "id": 657693,
+      "profile_use_background_image": true,
+      "listed_count": 324,
+      "profile_text_color": "666666",
+      "lang": "en",
+      "followers_count": 4993,
+      "protected": false,
+      "notifications": false,
+      "profile_background_image_url_https": "https://si0.twimg.com/images/themes/theme9/bg.gif",
+      "profile_background_color": "1A1B1F",
+      "verified": false,
+      "geo_enabled": true,
+      "time_zone": "GMT+2 ",
+      "description": "Banking Specialist at Twitter.\r\n\r\nInternet, opensource, media & geo/local geek.\r\n\r\nFollow @ezekiel for LANG=FR.",
+      "default_profile_image": false,
+      "profile_background_image_url": "http://a0.twimg.com/images/themes/theme9/bg.gif",
+      "statuses_count": 8504,
+      "friends_count": 2743,
+      "following": true,
+      "screen_name": "ambassador_muntony"
+    }
+]    
 
-
+   #Thank you!
 
 
 
